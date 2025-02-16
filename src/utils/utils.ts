edceea824a2022f47ac3fc42491d0dfede4c092b
EVGENII <ovr58armee@yandex.ru>
2025-02-16 23:55:18 +0400

    Add functions to fetch bookmarks and videos, and enhance Spotify video ID retrieval

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,17 @@
+interface ActiveTab {
+    id: number;
+}
+
+interface ScriptResult {
+    frameId: number;
+    result: string | null;
+}
+
+interface Video {
+    videoId: string;
+    title: string;
+}
+
 export async function getCurrentTab() {
     const tabs = await chrome.tabs.query({ active: true, currentWindow: true })
     return tabs[0]
@@ -26,6 +40,46 @@ export function fetchAllowedUrls() {
     })
 }
 
+export function fetchBookmarks(videoId: string) {
+    return new Promise((resolve) => {
+    chrome.storage.sync.get([videoId], (obj) => {
+        resolve(obj[videoId] ? JSON.parse(obj[videoId]) : [])
+    })
+})
+}
+
+export function fetchVideosWithBookmarks(videoId: string): Promise<Video[][]> {
+    return new Promise((resolve) => {
+        chrome.storage.sync.get(null, (obj: { [key: string]: string }) => {
+            console.log('POPUP - Fetch Videos:', obj, videoId);
+            const videos: Video[][] = [];
+            Object.keys(obj).forEach(key => {
+                const video: Video[] = JSON.parse(obj[key]);
+                console.log('POPUP - Video:', key, video);
+                if (key === videoId && video.length === 0) {
+                    const curVideo: Video[] = [{
+                        videoId: key,
+                        title: chrome.i18n.getMessage('currentVideo')
+                    }];
+                    videos.push(curVideo);
+                } else if (key !== videoId && video.length === 0) {
+                    chrome.storage.sync.remove(key);
+                } else if (video.length > 0 && key !== 'allowedUrls') {
+                    videos.push(video);
+                }
+            });
+            if (!Object.keys(obj).includes(videoId) && videoId) {
+                const curVideo: Video[] = [{
+                    videoId: videoId,
+                    title: chrome.i18n.getMessage('currentVideo')
+                }];
+                videos.push(curVideo);
+            }
+            resolve(videos);
+        });
+    });
+}
+
 export function getUrlParams(url: string, allowedUrls: string[]): string | null {
     let urlParams: string | null = null;
     if (url.includes('www.youtube.com/watch')) {
@@ -46,4 +100,24 @@ export function getUrlParams(url: string, allowedUrls: string[]): string | null 
         urlParams = 'technical';
     }
     return urlParams;
+}
+
+export async function getSpotifyVideoId(activeTab: ActiveTab): Promise<string | null> {
+    const results = await chrome.scripting.executeScript({
+        target: { tabId: activeTab.id, allFrames: true },
+        func: (): string | null => {
+            const idElement = document.querySelectorAll('a[data-testid="context-item-link"]')[0] as HTMLAnchorElement | undefined;
+            if (idElement) {
+                let idHref = idElement.href;
+                if (idHref.includes('album')) {
+                    idHref += `/${idElement.textContent?.replace(/ /g, '_')}`;
+                }
+                return idHref;
+            } else {
+                return null;
+            }
+        }
+    }) as ScriptResult[];
+    console.log('POPUP - Spotify Video Id:', results);
+    return results.flatMap(result => result.result ?? null).filter(Boolean)[0];
 }
